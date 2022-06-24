6c2c4ec961c3ad2dc59dfaacf88783235f0802a3
michael <shinlin09@gmail.com>
2022-06-24 19:30:27 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import Header from './Components/Header';
 import img1 from './assets/img/password-generator.png';
+import img2 from './assets/img/weather-api.png';
+import img3 from './assets/img/foodly.png';
+import img4 from './assets/img/restaurant-mockup.png';
+
+
 
 function App() {
   return (
@@ -12,9 +17,9 @@ function App() {
         <Nav />
         <Header />
         <Projects title="Password Generator" img={img1} githubLink="https://github.com/bonomichael98/javascript-password-generator" alt="password generator" type="Javascript" />
-        <Projects title="Password Generator" img={img1} githubLink="https://github.com/bonomichael98/javascript-password-generator" alt="password generator" type="Javascript" />
-        <Projects title="Password Generator" img={img1} githubLink="https://github.com/bonomichael98/javascript-password-generator" alt="password generator" type="Javascript" />
-        <Projects title="Password Generator" img={img1} githubLink="https://github.com/bonomichael98/javascript-password-generator" alt="password generator" type="Javascript" />
+        <Projects title="Weather API" img={img2} githubLink="https://bonomichael98.github.io/weather-api/" alt="weather api" type="Javascript" />
+        <Projects title="Foodly" img={img3} githubLink="https://foodly2022.herokuapp.com/" alt="foodly recipe sharing app" type="Node" />
+        <Projects title="Restaurant Mockup" img={img4} githubLink="https://bonomichael98.github.io/restaurant-project/" alt="restaurant project mockup" type="Javascript" />
         <About />
         <Contact address="Based in Mt.Carmel Ohio" email="bonomichael98@gmail.com" githubLink="https://github.com/bonomichael98" />
     </div>
--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -15,10 +15,10 @@ function Projects(props) {
         <section className="projects-section bg-light" id="projects">
             <div className="container px-4 px-lg-5">
                 <div className="row gx-0 mb-4 mb-lg-5 align-items-center">
-                    <div className="col-xl-8 col-lg-7"><img className="img-fluid mb-3 mb-lg-0" src={img} alt={alt} /><a href={githubLink}></a></div>
+                    <div className="col-xl-8 col-lg-7"><img className="img-fluid mb-3 mb-lg-0" src={img} alt={alt} /></div>
                     <div className="col-xl-4 col-lg-5">
                         <div className="featured-text text-center text-lg-left">
-                            <h4>{title}</h4>
+                            <h4><a href={githubLink}>{title}</a></h4>
                             <p className="text-black-50 mb-0">{type}</p>
                         </div>
                     </div>
